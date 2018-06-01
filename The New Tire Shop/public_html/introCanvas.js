634351e0bf66b5106403468fe0621b715a6b9d78
jjct1994 <jahir.1994.c@gmail.com>
2018-06-01 14:40:01 -0700

    Working project 2018

---
+++ The New Tire Shop/public_html/introCanvas.js
@@ -0,0 +1,17 @@
+/* Animated fire for wheel*/
+
+
+
+var canvas = document.querySelector("canvas");
+
+var xCenter = window.innerWidth/2;
+var yCenter = window.innerHeight/2;
+
+
+
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var ctx = canvas.getContext("2d");
+
